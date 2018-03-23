f908818f0bdd83def15b5e656689126f8d746f66
iO Ninja <ioninja14@gmail.com>
2018-03-23 13:03:35 +1100

    Removed key

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*
+{"commands":{"argstest":{"code":"channel.send(args);","info":""},"sum":{"code":" \nvar result = 0; \nfor (var i = 0; i < args.length; i++) { \n    result += Number(args[i]); \n} \nchannel.send(args.join(\" + \") + \" = \" + result); \n","info":""},"calc":{"code":" \nargs = message.content.split(\" \"); \nif (args[1] == \"+\") { \n    channel.send(Number(args[0]) + Number(args[2])); } \nelse if (args[1] == \"-\"){ \n    channel.send(Number(args[0]) - Number(args[2])); } \nelse if (args[1] == \"*\") { \n    channel.send(Number(args[0]) * Number(args[2])); } \nelse if (args[1] == \"/\") { \n    channel.send(Number(args[0]) / Number(args[2])); } \nelse if (args[1] == \"%\") { \n    channel.send(Number(args[0]) / Number(args[2])); } \nelse { \n    channel.send(\"wtf r u talking about\"); } \n","info":""},"remind":{"code":"var time = args[0]; \nargs.shift(); \nvar m = args.join(\" \"); \nsetTimeout(function() { \n    channel.send(m); \n}, time * 1000);","info":""}},"global":{"count":2}}
+*/
+
 var vm = require("vm");
 var Discord = require("discord.js");
 var client = new Discord.Client();
@@ -9,148 +13,153 @@ var commands = {};
 var globalData = {};
 
 function run(command, options, message) {
-	switch (command) {
-	case "run":
-		options.shift();
-		var code = extractCode(options.join(" "));
-		runScript(createScript(code, "<anonymous>"), message, null);
-		break;
-	case "create":
-		var name = options[0];
-		options.shift();
-		var code = extractCode(options.join(" "));
-		if (commandNames.indexOf(name) != -1) {
-			message.channel.send("Cannot create command `" + name + "`: it is a reserved name");
-		}
-		if (name in commands) {
-			message.channel.send("Cannot create command `" + name + "`: it already exists");
-		}
-		var success = true;
-		try {
-			commands[name] = {
-				script: createScript(code, name),
-				code: code,
-				info: ""
-			};
-		} catch(e) {
-			success = false;
-			message.channel.send("```" + formatError(e) + "```");
-		}
-		if (success) {
-			message.channel.send("Created command `" + name + "`");
-		}
-		break;
-	case "edit":
-		var name = options[0];
-		options.shift();
-		var code = extractCode(options.join(" "));
-		if (name in commands) {
+	try {
+		switch (command) {
+		case "run":
+			options.shift();
+			var code = extractCode(options.join(" "));
+			runScript(createScript(code, "<anonymous>"), message, null);
+			break;
+		case "create":
+			var name = options[0];
+			options.shift();
+			var code = extractCode(options.join(" "));
+			if (commandNames.indexOf(name) != -1) {
+				message.channel.send("Cannot create command `" + name + "`: it is a reserved name");
+			}
+			if (name in commands) {
+				message.channel.send("Cannot create command `" + name + "`: it already exists");
+			}
 			var success = true;
 			try {
-				commands[name].script = createScript(code, name);
-				commands[name].code = code;
+				commands[name] = {
+					script: createScript(code, name),
+					code: code,
+					info: ""
+				};
 			} catch(e) {
 				success = false;
 				message.channel.send("```" + formatError(e) + "```");
 			}
 			if (success) {
-				message.channel.send("Edited command `" + name + "`");
+				message.channel.send("Created command `" + name + "`");
 			}
-		} else {
-			message.channel.send("Command not found: `" + name + "`");
-		}
-		break;
-	case "show":
-		var name = options[0];
-		if (name in commands) {
-			message.channel.send("```JS\n" + commands[name].code + "\n```");
-		}
-		break;
-	case "info":
-		var name = options[0];
-		if (name in commands) {
-			if (options.length > 1) {
-				options.shift();
-				var info = options.join(" ").trim();
-				commands[name].info = info;
-				message.channel.send("Saved info for `" + name + "`");
+			break;
+		case "edit":
+			var name = options[0];
+			options.shift();
+			var code = extractCode(options.join(" "));
+			if (name in commands) {
+				var success = true;
+				try {
+					commands[name].script = createScript(code, name);
+					commands[name].code = code;
+				} catch(e) {
+					success = false;
+					message.channel.send("```" + formatError(e) + "```");
+				}
+				if (success) {
+					message.channel.send("Edited command `" + name + "`");
+				}
 			} else {
-				if (commands[name].info != "") {
-					message.channel.send("```" + name + ": " + commands[name].info + "```");
+				message.channel.send("Command not found: `" + name + "`");
+			}
+			break;
+		case "show":
+			var name = options[0];
+			if (name in commands) {
+				message.channel.send("```JS\n" + commands[name].code + "\n```");
+			}
+			break;
+		case "info":
+			var name = options[0];
+			if (name in commands) {
+				if (options.length > 1) {
+					options.shift();
+					var info = options.join(" ").trim();
+					commands[name].info = info;
+					message.channel.send("Saved info for `" + name + "`");
 				} else {
-					message.channel.send("No info set for `" + name + "`");
+					if (commands[name].info != "") {
+						message.channel.send("```" + name + ": " + commands[name].info + "```");
+					} else {
+						message.channel.send("No info set for `" + name + "`");
+					}
 				}
+			} else {
+				message.channel.send("Command not found: `" + name + "`");
 			}
-		} else {
-			message.channel.send("Command not found: `" + name + "`");
-		}
-		break;
-	case "delete":
-		var name = options[0];
-		if (name in commands) {
-			delete commands[name];
-			message.channel.send("Deleted command `" + name + "`");
-		} else {
-			message.channel.send("Command not found: `" + name + "`");
-		}
-		break;
-	case "export":
-		var data = {};
-		data.commands = {};
-		data.global = globalData;
-		for (var command in commands) {
-			data.commands[command] = {
-				code: commands[command].code,
-				info: commands[command].info
-			};
-		}
-		message.channel.send("```JSON\n" + JSON.stringify(data) + "\n```");
-		break;
-	case "import":
-		var data = JSON.parse(options.join(" "));
-		for (var command in data.commands) {
-			try {
-				commands[command] = {
-					script: createScript(data.commands[command], command),
-					code: data.commands[command].code,
-					info: data.commands[command].info
-				};
-			} catch(e) {
-				message.channel.send("Error when importing `" + command + "`\n```" + formatError(e) + "```");
+			break;
+		case "delete":
+			var name = options[0];
+			if (name in commands) {
+				delete commands[name];
+				message.channel.send("Deleted command `" + name + "`");
+			} else {
+				message.channel.send("Command not found: `" + name + "`");
 			}
-			for (var g in data.global) {
-				globalData[g] = data.global[g];
-			}
-		}
-		message.channel.send("Imported data");
-		break;
-	case "list":
-		if (Object.keys(commands).length > 0) {
-			var string = "```\n";
+			break;
+		case "export":
+			var data = {};
+			data.commands = {};
+			data.global = globalData;
 			for (var command in commands) {
-				string += command;
-				if (commands[command].info != "") {
-					string += ": " + commands[command].info;
-				}
-				string += "\n";
+				data.commands[command] = {
+					code: commands[command].code,
+					info: commands[command].info
+				};
 			}
-			string += "```";
-			message.channel.send(string);
-		} else {
-			message.channel.send("No commands found");
+			message.channel.send("```JSON\n" + JSON.stringify(data) + "\n```");
+			break;
+		case "import":
+			var data = JSON.parse(options.join(" "));
+			for (var command in data.commands) {
+				try {
+					commands[command] = {
+						script: createScript(data.commands[command], command),
+						code: data.commands[command].code,
+						info: data.commands[command].info
+					};
+				} catch(e) {
+					message.channel.send("Error when importing `" + command + "`\n```" + formatError(e) + "```");
+				}
+				for (var g in data.global) {
+					globalData[g] = data.global[g];
+				}
+			}
+			message.channel.send("Imported data");
+			break;
+		case "list":
+			if (Object.keys(commands).length > 0) {
+				var string = "```\n";
+				for (var command in commands) {
+					string += command;
+					if (commands[command].info != "") {
+						string += ": " + commands[command].info;
+					}
+					string += "\n";
+				}
+				string += "```";
+				message.channel.send(string);
+			} else {
+				message.channel.send("No commands found");
+			}
+			break;
+		case "help":
+			message.channel.send("Help page coming soon!");
+			break;
+		default:
+			if (command in commands) {
+				message.content = options.join(" ");
+				runScript(commands[command].script, message, command);
+			} else {
+				message.channel.send("Command not found: `" + command + "`");
+			}
+			break;
 		}
-		break;
-	case "help":
-		message.channel.send("Help page coming soon!");
-		break;
-	default:
-		if (command in commands) {
-			message.content = options.join(" ");
-			runScript(commands[command].script, message, command);
-		} else {
-			message.channel.send("Command not found: `" + command + "`");
-		}
-		break;
+	} catch(e) {
+		console.log(e);
+		message.channel.send("Malformed input");
 	}
 }
 
@@ -171,7 +180,7 @@ client.on("message", function(message) {
 	}
 });
 
-client.login("NDI2MjM1NzMzNTk0OTk2NzQ4.DZTCgA.U_ByXjdS0bP6ucKxvdqGlEdoJ5I");
+client.login(process.argv[2]);
 
 function exitHandler() {
 	var data = {};
@@ -209,6 +218,8 @@ function runScript(script, message, command) {
 		script.runInNewContext({
 			command: command,
 			message: message,
+			text: message.content,
+			args: message.content.split(" "),
 			guild: message.channel.guild,
 			channel: message.channel,
 			user: message.author,
